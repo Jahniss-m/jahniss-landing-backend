d403559b81e0297f3847fd7b4946b9fe91d6ae89
Jahniss-m <soledadsatide1979@gmail.com>
2025-07-02 13:05:18 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,16 +1,29 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./messages.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Aquí creas todas las tablas que estén definidas en los modelos
+# Definición del modelo Message
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+
+# Creación de las tablas en la base de datos
 Base.metadata.create_all(bind=engine)
 
+# Función para obtener la sesión de base de datos
 def get_db():
     db = SessionLocal()
     try:
